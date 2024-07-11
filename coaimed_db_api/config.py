93869d2efddd8d3c96e6989589dea13a0cbbb98b
IvanaRagusa <tdp.ragusa@gmail.com>
2024-07-11 17:53:41 +0200

    oki

--- coaimed_db_api/config.py
+++
@@ -1,7 +0,0 @@
-# config.py
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://myuser:mypassword@localhost/mydatabase')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
